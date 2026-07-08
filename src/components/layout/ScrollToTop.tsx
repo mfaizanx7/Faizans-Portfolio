c336ea30592f3d7160c2c73eadd0ed40dcce85d5
Muhammad Faizan Khan <m.faizanx7@gmail.com>
2026-07-08 19:20:52 +0500

    icon fix

--- src/components/layout/ScrollToTop.tsx
+++ src/components/layout/ScrollToTop.tsx
@@ -1,6 +1,10 @@
 import { useEffect } from 'react'
 import { useLocation } from 'react-router-dom'
 
+if ('scrollRestoration' in window.history) {
+  window.history.scrollRestoration = 'manual'
+}
+
 export function ScrollToTop() {
   const { pathname } = useLocation()
   useEffect(() => {
